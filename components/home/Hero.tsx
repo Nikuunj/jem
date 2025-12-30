78edff8af764bf605964fbf269f3a8372b116175
Nikunj <mnikunj262@gmail.com>
2025-12-31 01:03:25 +0530

    update bgof hero

---
+++ components/home/Hero.tsx
@@ -0,0 +1,13 @@
+import HeroBG from "../ui/HeroBG"
+
+function Hero() {
+  return (
+    <div className="text-white bg-[#020617] h-full min-h-screen">
+      <HeroBG />
+      MOVING EARTH
+      BUILDING FUTURES
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/ui/HeroBG.tsx
@@ -0,0 +1,7 @@
+
+function HeroBG() {
+  return (
+    < div className="absolute inset-0 z-0 bg-[radial-gradient(circle_500px_at_50%_20%,rgba(251,191,36,0.4),transparent)]" />
+  )
+}
+export default HeroBG
