9edef5abf407c8808d1afbb3149b653e523f3657
Nikunj <mnikunj262@gmail.com>
2026-01-02 17:49:41 +0530

    update: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/home/Hero";
-import HeroBG from "@/components/ui/HeroBG";
 
 export default function Home() {
   return (
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -2,11 +2,27 @@ import HeroBG from "../ui/HeroBG"
 
 function Hero() {
   return (
-    <div className="text-white bg-[#020617] h-full min-h-screen">
+    <div className="text-white bg-[#020617] h-full min-h-screen flex flex-col justify-center  px-7 gap-5">
       <HeroBG />
-      MOVING EARTH
-      BUILDING FUTURES
+      <span className="bg-yellow-400 text-black w-fit py-2 px-4 text-md -skew-x-8 ms-2 font-medium">
+        PROFESSIONAL EARTHMOVING
+      </span>
+      <div className="">
+        <TextColor first="MOVING" second="EARTH" />
+        <TextColor first="BUILDING" second="FUTURES" />
+      </div>
+      <p className="text-lg/7 text-zinc-400 max-w-4xl ">
+        Janvi Earth Mover delivers powerful excavation and earthmoving solutions. From foundation work to large-scale land development, we get the job done.
+      </p>
     </div>
+  )
+}
+
+function TextColor({ first, second }: { first: string, second: string }) {
+  return (
+    <p className="text-[81px]/21 font-bold">
+      <span>{first}</span> <span className="text-yellow-400">{second}</span>
+    </p>
   )
 }
 
