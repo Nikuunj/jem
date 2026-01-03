68dc0f0171c850c3b0c866574595b9c2ed2b1073
Nikunj <mnikunj262@gmail.com>
2026-01-03 18:02:04 +0530

    update : bg to noice bg

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,29 +4,30 @@ import { ArrowRight } from "lucide-react"
 
 function Hero() {
   return (
-    <div className="text-white bg-[#020617] h-full min-h-screen flex flex-col justify-center  px-7 gap-5">
+    <div className="text-white  h-full min-h-screen flex flex-col justify-center  px-7 gap-5">
       <HeroBG />
-      <span className="bg-yellow-400 text-black w-fit py-2 px-4 text-md -skew-x-8 ms-2 font-medium">
+      <span className="bg-yellow-400 text-black w-fit py-2 px-4 text-sm sm:text-md -skew-x-8 ms-2 font-medium">
         PROFESSIONAL EARTHMOVING
       </span>
       <div className="">
         <TextColor first="MOVING" second="EARTH" />
         <TextColor first="BUILDING" second="FUTURES" />
       </div>
-      <p className="text-lg/7 text-zinc-400 max-w-4xl ">
+      <p className="text-sm/6 md:text-lg/7 text-zinc-400 max-w-4xl ">
         Janvi Earth Mover delivers powerful excavation and earthmoving solutions. From foundation work to large-scale land development, we get the job done.
       </p>
-      <div className="gap-4  flex items-center  ">
-        <span className="bg-yellow-400 text-black w-fit py-3 px-7    font-medium flex items-center">
-          <span className="text-xl">
-            {PHONE_NUMBER}
-          </span> <ArrowRight />
-        </span>
-        <span className="border-2 border-yellow-400  w-fit py-3 px-7   font-medium flex items-center">
-          <span className="text-xl">
+      <div className="gap-4  flex sm:items-center flex-col sm:flex-row ">
+        <span className="bg-yellow-400 text-black w-fit py-3 px-7    font-medium flex items-center gap-2">
+          <span className="text-sm sm:text-xl">
             {PHONE_NUMBER}
           </span>
-          <ArrowRight />
+          <ArrowRight className="size-3 sm:size-5" />
+        </span>
+        <span className="border-2 border-yellow-400  w-fit py-3 px-7   font-medium flex items-center gap-2">
+          <span className="text-sm sm:text-xl">
+            {PHONE_NUMBER}
+          </span>
+          <ArrowRight className="size-3 sm:size-5" />
         </span>
       </div>
     </div>
@@ -35,7 +36,7 @@ function Hero() {
 
 function TextColor({ first, second }: { first: string, second: string }) {
   return (
-    <p className="text-[81px]/21 font-bold">
+    <p className="text-3xl sm:text-5xl lg:text-[81px]/21 font-bold">
       <span>{first}</span> <span className="text-yellow-400">{second}</span>
     </p>
   )
--- components/ui/HeroBG.tsx
+++ components/ui/HeroBG.tsx
@@ -1,7 +1,19 @@
 
 function HeroBG() {
   return (
-    < div className="absolute inset-0 z-0 bg-[radial-gradient(circle_500px_at_50%_20%,rgba(251,191,36,0.4),transparent)]" />
-  )
+    <div className="absolute inset-0 -z-10">
+      <div className="absolute inset-0 bg-radial-[at_20%_5%] from-zinc-950 from-35% via-50% to-65% via-yellow-800 to-zinc-950 " />
+
+      <div
+        className="absolute inset-0 opacity-50 "
+        style={{
+          backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
+          backgroundRepeat: 'repeat',
+          mixBlendMode: 'overlay'
+        }}
+      />
+    </div>
+  );
 }
-export default HeroBG
+
+export default HeroBG;
