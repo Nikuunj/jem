34872d96c89352ea9a6ecaf916e59ef7d0c94b31
Nikunj <mnikunj262@gmail.com>
2026-01-03 15:46:01 +0530

    make it re-usesble

---
+++ components/ui/TitleDom.tsx
@@ -0,0 +1,21 @@
+function TitleDom({ name, line1, line2, theme = "w" }: { name: string, line1: string, line2: string, theme?: "w" | "b" }) {
+  const style = {
+    "w": "text-black",
+    "b": "text-white"
+  }
+  return (
+    <div className="flex flex-col items-center gap-4 text-center text-balance">
+      <h1 className="text-blue-400">
+        {name}
+      </h1>
+      <p className={`text-4xl font-medium  max-w-lg ${style[theme]}`}>
+        {line1}
+      </p>
+      <p className="text-base  text-zinc-400 max-w-lg  ">
+        {line2}
+      </p>
+    </div>
+  )
+}
+
+export default TitleDom
