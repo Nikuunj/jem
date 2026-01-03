be76100a4e0848deba7ef24c05ab086c8d03c977
Nikunj <mnikunj262@gmail.com>
2026-01-03 15:45:25 +0530

    update: work section

---
+++ components/home/Work.tsx
@@ -0,0 +1,18 @@
+import { recent_project } from "@/lib/data"
+import TitleDom from "../ui/TitleDom"
+import { AnimatedTestimonials } from "../ui/AnimatedTestimoials"
+
+function Work() {
+  return (
+    <div className="flex flex-col gap-9 bg-black  py-13">
+      <TitleDom name={"OUR WORK"}
+        theme="b"
+        line1={"RECENT PROJECTS"}
+        line2={"Take a look at some of our completed earthmoving and excavation projects."} />
+
+      <AnimatedTestimonials testimonials={recent_project} />
+    </div>
+  )
+}
+
+export default Work
