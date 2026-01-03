c2b8d62bb0ecac0d67e931ba6dfb2e14ea27e271
Nikunj <mnikunj262@gmail.com>
2026-01-03 15:47:13 +0530

    update: page and infinity more testimonal

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/Hero";
 import Service from "@/components/home/Service";
+import Work from "@/components/home/Work";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-17">
       <Hero />
       <Service />
+      <Work />
     </div>
   );
 }
