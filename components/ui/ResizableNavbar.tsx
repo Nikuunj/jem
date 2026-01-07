3ddaf48f9f413c54914b6da0343a0e00344b6e0d
Nikunj <mnikunj262@gmail.com>
2026-01-08 01:42:25 +0530

    update: name of startup and logo

--- components/ui/ResizableNavbar.tsx
+++ components/ui/ResizableNavbar.tsx
@@ -246,12 +246,12 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img
-        src="https://assets.aceternity.com/logo-dark.png"
+        src="/logo.png"
         alt="logo"
-        width={30}
-        height={30}
+        width={80}
+        height={20}
       />
-      <span className="font-medium text-black dark:text-white">Startup</span>
+      <span className="font-medium text-black dark:text-white">Janvi Earth Movers</span>
     </a>
   );
 };
