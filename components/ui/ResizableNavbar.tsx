bddc178a55a373f0f11417a72ef1f1a9e89ba448
Nikunj <mnikunj262@gmail.com>
2026-01-08 12:06:30 +0530

    update: nav style fix

--- components/ui/ResizableNavbar.tsx
+++ components/ui/ResizableNavbar.tsx
@@ -96,10 +96,10 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
   return (
     <motion.div
       animate={{
-        backdropFilter: visible ? "blur(10px)" : "none",
-        boxShadow: visible
-          ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
-          : "none",
+        backdropFilter: "blur(10px)",
+        boxShadow:
+          "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
+        ,
         width: visible ? "40%" : "100%",
         y: visible ? 20 : 0,
       }}
@@ -243,7 +243,7 @@ export const NavbarLogo = () => {
   return (
     <a
       href="#"
-      className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
+      className="relative z-20 mr-4 flex items-center space-x-2  text-sm font-normal text-black"
     >
       <img
         src="/logo.png"
